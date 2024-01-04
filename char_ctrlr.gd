84ba2b24ec8ef00ab1aae58d5b2f1c58003a6c43
N07H1NG <nikitos09012004@mail.ru>
2024-01-04 23:32:31 +0300

    crouch
    crouch

--- char_ctrlr.gd
+++ char_ctrlr.gd
@@ -1,15 +1,17 @@
 extends CharacterBody3D
 
 
-@export var walk_speed = 3.0
+@export var walk_speed = 4.0
 @export var sprint_speed = 7.0
+@export var crouch_walk_speed = 2.0
+@export var crouch_sprint_speed = 3.5
 @export var jump_velocity = 4.5
-@export var sensitivity = Vector2(1.0,1.0)
+@export var sensitivity = Vector2(0.6,0.8)
 @export var camera_responsive = Vector2(7.0,7.0)
 @export var air_control = 0.0
 @export var accel = 16.0
-@onready var head = $Node3D
-@onready var headbob = $Node3D/headbobcentre
+@onready var head = $head
+@onready var headbob = $head/headbobcentre
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var look_rot : Vector2
@@ -17,6 +19,10 @@ var look_target : Vector2
 var current_speed = walk_speed
 var target_speed = walk_speed
 var head_bob_timer = 0.0
+var head_height_default = 1.5
+var head_height_crouch = 0.7
+var crouched = false
+var sprinting = false
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
@@ -27,13 +33,11 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = jump_velocity
-	if Input.is_action_just_pressed("Sprint"):
-		target_speed = sprint_speed
-	if Input.is_action_just_released("Sprint"):
-		target_speed = walk_speed 
 	
+		
+		
+	
+	head.position.y = lerp(head.position.y,float(crouched)*head_height_crouch+float(!crouched)*head_height_default,delta*5)
 	current_speed = lerp(current_speed,target_speed,accel*delta*0.3)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -61,11 +65,37 @@ func _process(delta):
 	
 	
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		look_target.y -= event.relative.x*sensitivity.x
 		look_target.x -= event.relative.y*sensitivity.y
 		look_target.x = clamp(look_target.x,-80,90)	
-		#look_target.y = fmod(look_target.y,360.0)
-
+	
+	if event.is_action_pressed("Exit"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if event.is_action_pressed("Return") and Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	if event.is_action_pressed("Jump") and is_on_floor():
+		velocity.y = jump_velocity
+	if event.is_action_pressed("Sprint"):
+		sprinting = true
+		if !crouched:
+			target_speed = sprint_speed
+	if event.is_action_released("Sprint"):
+		sprinting = false
+		if crouched:
+			target_speed = crouch_walk_speed
+		else:
+			target_speed = walk_speed
+	if event.is_action_pressed("Crouch"):
+		crouched = true
+		target_speed = crouch_walk_speed
+	if event.is_action_released("Crouch"):
+		crouched = false
+		if sprinting:
+			target_speed = sprint_speed
+		else:
+			target_speed = walk_speed
 	
 	

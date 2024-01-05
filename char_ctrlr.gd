e5c18ec2526c8cf30cb734726b85cd1ba97dbf3e
N07H1NG <nikitos09012004@mail.ru>
2024-01-05 05:30:52 +0300

    some fov shenanigans and jump-sensitive headbobbing
    i fucking love this fpc idc that we don't need it

--- char_ctrlr.gd
+++ char_ctrlr.gd
@@ -5,10 +5,10 @@ extends CharacterBody3D
 @export var sprint_speed = 7.0
 @export var crouch_walk_speed = 2.0
 @export var jump_velocity = 5.0
-@export var sensitivity = Vector2(0.2,0.4)
+@export var sensitivity = Vector2(0.25,0.4)
 @export var camera_responsive = Vector2(7.0,7.0)
 @export var air_control = 0.0
-@export var accel = 5.0
+@export var accel = 5.5
 @onready var head = $head
 @onready var headbob = $head/headbobcentre
 @onready var collider = $CollisionShape3D
@@ -19,14 +19,13 @@ var look_target : Vector2
 var current_speed = walk_speed
 var target_speed = walk_speed
 var head_bob_timer = 0.0
-var head_height_default = 1.5
-var head_height_crouch = 0.7
 var crouched = false
 var crouch_input = false
 var sprinting = false
 var crouch_height = 1.0
 var stand_height
 var lean = 0
+var vert_offset = 0.0
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	stand_height = collider.shape.height
@@ -37,12 +36,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	
-		
-		
-	
-	head.position.y = lerp(head.position.y,float(crouched)*head_height_crouch+float(!crouched)*head_height_default,delta*5)
 	if crouch_input and !crouched:
 		crouched = is_on_floor()
 	if !crouch_input and crouched:
@@ -70,12 +63,18 @@ func _physics_process(delta):
 	head.rotation_degrees.x = look_rot.x
 	rotation_degrees.y = look_rot.y
 	get_lean(delta)
+	determine_speed()
+	camera_fov_control()
 	
 		
 func _process(delta):
 	if is_on_floor():
 		head_bob_timer += delta*velocity.length()*3.0
 	headbob.position.y = sin(head_bob_timer)*0.1
+	vert_offset = lerp(vert_offset,clamp(velocity.y,-10.0,6)*0.07,delta*4.0)
+	headbob.position.y += vert_offset*1.6
+	headbob.rotation_degrees.x = vert_offset*9
+	
 		
 	
 	
@@ -95,32 +94,21 @@ func _input(event):
 		velocity.y = jump_velocity
 	if event.is_action_pressed("Sprint"):
 		sprinting = true
-		if !crouched:
-			target_speed = sprint_speed
 	if event.is_action_released("Sprint"):
 		sprinting = false
-		if crouched:
-			target_speed = crouch_walk_speed
-		else:
-			target_speed = walk_speed
 	if event.is_action_pressed("Crouch"):
 		if is_on_floor(): crouched = true
 		crouch_input = true
-		target_speed = crouch_walk_speed
 	if event.is_action_released("Crouch"):
 		crouch_input = false
 		crouched = is_under_smth()
-		if sprinting and !crouched:
-			target_speed = sprint_speed
-		elif !crouched:
-			target_speed = walk_speed
 	
 func get_crouch(delta : float, crouching = false):
 	var target_height : float = crouch_height if crouching else stand_height
 	
 	collider.shape.height = lerp(collider.shape.height, target_height, delta)
 	collider.position.y = lerp(collider.position.y, target_height * 0.5, delta)
-	head.position.y = collider.shape.height-0.5
+	head.position.y = collider.shape.height-0.2
 
 func is_under_smth()->bool:
 	return $CollisionShape3D/Area3D.has_overlapping_bodies() and is_on_floor()
@@ -129,6 +117,17 @@ func get_lean(delta):
 	var can_lean_right = float(!$CollisionShape3D/right_ear.has_overlapping_bodies() and $CollisionShape3D/front.has_overlapping_bodies())
 	var can_lean_left = float(!$CollisionShape3D/left_ear.has_overlapping_bodies() and $CollisionShape3D/front.has_overlapping_bodies())
 	var target_lean : float = float(Input.is_action_pressed("LeanRight"))*can_lean_right - float(Input.is_action_pressed("LeanLeft"))*can_lean_left
-	print(target_lean)
 	head.position.x = lerp(head.position.x, 0.7*target_lean, delta*(4.0 - abs(target_lean)))
 	head.rotation_degrees.z = lerp(head.rotation_degrees.z, -20*target_lean, delta*(4.0 - abs(target_lean)))
+
+func determine_speed():
+	if crouched:
+		target_speed = crouch_walk_speed
+	elif sprinting:
+		target_speed = sprint_speed
+	else:
+		target_speed = walk_speed
+		
+func camera_fov_control():
+	$head/headbobcentre/Camera3D.set_fov(70+0.2*Vector2(velocity.z,velocity.x).length()*current_speed + 0.4*abs(head.rotation_degrees.z))
+		
